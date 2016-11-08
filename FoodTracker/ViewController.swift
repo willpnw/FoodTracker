748a8f2e39d6665294e244a08dc8a8eb158d0eaa
will patterson <wpatterson@wills-iMac.local>
2016-11-08 13:33:58 -0800

    Checkpoint Work with View Controllers

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var photoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,14 +32,34 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     // MARK: Actions
+    @IBAction func selectImageFromPhotoLibrary(_ sender: UITapGestureRecognizer) {
+        print("SELECT")
+        nameTextField.resignFirstResponder()
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self
+        present(imagePickerController, animated: true, completion: nil)
+    }
+
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        print("DEFAULT")
         mealNameLabel.text = "Default Text"
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
+        print("DONE")
         mealNameLabel.text = textField.text
     }
     
-
+    // MARK: UIImagePickerControllerDelegate
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func  imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        photoImageView.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
 }
 
