470cc8d1d3a8caa695468d326811eac7826fcb21
will patterson <wpatterson@wills-iMac.local>
2016-11-08 15:56:10 -0800

    Checkpoint Define Your Data Model

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,27 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by will patterson on 11/8/16.
+//  Copyright © 2016 TableSafe. All rights reserved.
+//
+
+import UIKit
+
+class Meal {
+    // MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+}
--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -33,4 +33,17 @@ class FoodTrackerTests: XCTestCase {
         }
     }
     
+    // MARK: FoodTracker Tests
+    func testMealInitialization() {
+        let potentialItem = Meal(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssert((potentialItem != nil))
+        
+        let noName = Meal(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName, "Empty name is invalid")
+        
+        let barRating = Meal(name: "Really bad Rating", photo: nil, rating: -1)
+        XCTAssertNil(barRating)
+    }
+    
+    
 }
